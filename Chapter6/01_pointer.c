052634115d9ddf7c263083776104b791bfe0f7a2
MKAirborne <mnkpnsr@gmail.com>
2025-03-14 11:24:43 +0530

    updated

--- Chapter6/01_pointer.c
+++ Chapter6/01_pointer.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    int i = 72;
+    int i = 75;
     int* j = &i; // j is a pointer pointing to i
     printf("The address of i is %p\n", &i);
     printf("The address of i is %p\n", j);
